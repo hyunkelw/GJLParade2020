bc8eb0f577d26379e64aae428a424e5d618b8156
redd096 <martin.babino96@gmail.com>
2020-10-11 22:30:21 +0200

    cadono cose e le colpisci

---
+++ Assets/Scripts/Bat.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+    [Header("Rotate Bat")]
+    [Tooltip("Y axis to negative")] [SerializeField] float left = -75;
+    [Tooltip("Y axis to positive")] [SerializeField] float right = 75;
+    [Tooltip("X axis to negative")] [SerializeField] float up = -50;
+    [Tooltip("X axis to positive")] [SerializeField] float down = 30;
+
+    Camera cam;
+    Rigidbody rb;
+
+    void Start()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Mouse0) == false)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = transform.position.z;
+            Vector3 worldPosition = cam.ScreenToWorldPoint(mousePosition);
+
+            Debug.Log(mousePosition + " -- " + worldPosition);
+
+            //move position
+            Vector3 position = new Vector3(worldPosition.x, worldPosition.y, transform.position.z);
+            //rb.MovePosition(position);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            //mouse position to viewport (from 0,0 to 1,1)
+            Vector3 viewportPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+
+            //from left to right, from down to up
+            float leftRight = Mathf.Lerp(left, right, viewportPosition.x);
+            float upDown = Mathf.Lerp(down, up, viewportPosition.y);
+
+            //rotate X and Y axis
+            Vector3 euler = new Vector3(upDown, leftRight, 0);
+            //rb.rotation = Quaternion.Euler(euler);
+            rb.MoveRotation(Quaternion.Euler(euler));
+            //transform.eulerAngles = new Vector3(upDown, leftRight, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] Collider area = default;
+    [SerializeField] GameObject[] prefabs = default;
+    [SerializeField] float delay = 1.5f;
+
+    float time;
+
+    void Update()
+    {
+        if(Time.time > time)
+        {
+            time = Time.time + delay;
+            CreateObject();
+        }
+    }
+
+    void CreateObject()
+    {
+        //random position inside the area
+        Vector3 center = area.bounds.center;
+        Vector3 halfSize = area.bounds.extents;
+
+        float x = Random.Range(center.x - halfSize.x, center.x + halfSize.x);
+        float y = Random.Range(center.y - halfSize.y, center.y + halfSize.y);
+        float z = Random.Range(center.z - halfSize.z, center.z + halfSize.z);
+
+        //create object
+        GameObject obj = Instantiate(prefabs[Random.Range(0, prefabs.Length)]);
+        obj.transform.position = new Vector3(x, y, z);
+        obj.transform.rotation = Random.rotation;
+
+        Destroy(obj, 15);
+    }
+}
