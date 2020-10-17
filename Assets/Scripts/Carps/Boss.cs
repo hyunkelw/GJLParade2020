6ec2148024e161b55a63990cd0d7ba79771cd133
redd096 <martin.babino96@gmail.com>
2020-10-17 18:32:07 +0200

    nello script Idolo bisogna decidere come spawnare il boss e poi settare lo script Boss

---
+++ Assets/Scripts/Carps/Boss.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    //deve spawnare fuori mappa e avvicinarsi per affacciarsi al cono
+    //il rigidbody può essere rimosso o reso kinematic o lockare posizioni e rotazioni
+    //deve buttare fuori carpe esplosive (rimuovi le aree che ci sono ora in FallManager e aggiungerne una che si trova nella bocca della carpa?)
+    //quando muore deve resettare il rigidbody per tornare normale e aggiungere un AddForce per spararlo via
+    //per sbloccare la mazza deve fare PlayerPrefs.SetInt("Boss Killed", 1);
+}
