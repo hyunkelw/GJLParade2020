eaf6a832e4e099f43bd05d19720c8f30d9adfb34
redd096 <martin.babino96@gmail.com>
2020-10-17 18:25:54 +0200

    aggiunto POW e idoli

---
+++ Assets/Scripts/Idolo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idolo : MonoBehaviour
+{
+    static List<Idolo> idolosInScene = new List<Idolo>();
+
+    void Start()
+    {
+        //add to the list
+        idolosInScene.Add(this);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //if hit by a carp, remove from the list
+        if(idolosInScene.Contains(this) && collision.gameObject.GetComponent<Carp>())
+        {
+            idolosInScene.Remove(this);
+
+            //and try activate boss
+            TryActivateBoss();
+        }
+    }
+
+    void TryActivateBoss()
+    {
+        //if there are no more idolos in scene
+        if(idolosInScene == null || idolosInScene.Count <= 0)
+        {
+            //activate boss
+        }
+    }
+}
