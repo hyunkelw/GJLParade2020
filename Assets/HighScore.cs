5e0a09f577f8470ba4b218d28f7af9717a02d120
Willy Utano <Willy.utano@gmail.com>
2020-10-18 23:22:39 +0200

    High Score

---
+++ Assets/HighScore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TextMesh>().text = $"HIGH SCORE:\n {PlayerPrefs.GetInt("High Score")}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
